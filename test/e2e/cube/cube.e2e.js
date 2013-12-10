0020d85f8f5818daa228725ac84fa72bedbdfbd8
Robin Böhm <robinboehm@googlemail.com>
2013-12-10 11:55:24 +0100

    add(index): add html-code for cube

--- test/e2e/cube/cube.e2e.js
+++ test/e2e/cube/cube.e2e.js
@@ -12,4 +12,16 @@ describe("A initial project", function(){
         expect(element('link').attr('href')).toBe('styles/cube.css');
     });
 
+    it("should contain cube html-elements",function(){
+        expect(element('div.wrap').html()).toBeDefined();
+        expect(element('div.wrap > .cube').html()).toBeDefined();
+        expect(repeater('div.wrap > .cube >').count()).toBe(6);
+        expect(element('div.wrap > .cube > .left').html()).toBeDefined();
+        expect(element('div.wrap > .cube > .right').html()).toBeDefined();
+        expect(element('div.wrap > .cube > .top').html()).toBeDefined();
+        expect(element('div.wrap > .cube > .bottom').html()).toBeDefined();
+        expect(element('div.wrap > .cube > .front').html()).toBeDefined();
+        expect(element('div.wrap > .cube > .back').html()).toBeDefined();
+    });
+
 });
